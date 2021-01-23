cc4e92fa391769c99cd3b82a77989f6680b60d2f
Tyler Silverman <silverman.tyler@gmail.com>
2021-01-23 12:24:35 -0500

    added mongo and heruko links

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const logger = require("morgan");
-const mongoose = require("mongoose");
+const mongoose = require("mongoose");//connecting to mongoose sever database
 const compression = require("compression");
 
-const PORT = 4758;
+const PORT = 7632;
 
 const app = express();
 
@@ -15,14 +15,18 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+//connecting to mongoose sever database 
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
 
 // routes
 app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
+  console.log("Now listening http://localhost:" + PORT);
 });
